c653fe76898477b15af16af2b63a95d1ca41b357
Marc Jardine Esperas <marc.esperas@gmail.com>
2022-11-17 14:13:07 +0000

    Added handler for no items

---
+++ ToDoList/ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,63 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Marc Jardine Esperas on 11/8/22.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView(), label: {
+                    Text("Add Something")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal, 30)
+                .shadow(radius: 10)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+    }
+}
